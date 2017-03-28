5112b0f2f823caf0255d6fbe9bb9c8d00c3df62d
yunhan.zheng <hz11z20255@126.com>
2017-03-28 13:12:59 -0400

    config webpack with express; create entry point

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,18 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
 const port = 3000;
 const app = express();
+const compiler = webpack(config);
+
+//tell express to use webpack-dev-middleware
+app.use(require('webpack-dev-middleware')(compiler,{
+  noInfo:true,
+  publicPath:config.output.publicPath
+}));
 
 //any reference to the root should be handled by the funtion
 //declare routing
