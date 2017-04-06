0b69d6a659096249b7c76607015e7891e6aa6248
yunhan.zheng <hz11z20255@126.com>
2017-04-06 15:53:21 -0400

    dom testing

---
+++ buildScripts/testSetup.js
@@ -0,0 +1,7 @@
+//This file isn't transpiles, so must use CommonJS and ES5
+
+//Register babel to transpile before the tests run
+require("babel-register")();
+
+//Disable webpack features that Mocha does not understand
+require.extensions['.css']=function(){};
