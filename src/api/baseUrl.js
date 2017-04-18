6cd079b3e7493855c901c10ca750f503bf78ff34
yunhan.zheng <hz11z20255@126.com>
2017-04-18 13:15:46 -0400

    point to production API on heroku

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl(){
-  return getQueryStringParameterByName('useMockApi')?'http://localhost:3001/':'/';
+  return getQueryStringParameterByName('useMockApi')?'http://localhost:3001/':'https://rocky-mesa-61413.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
